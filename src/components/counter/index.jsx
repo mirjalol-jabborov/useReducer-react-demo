13ebb66f0a5a94205eb09ec45f0b048ac00cca2a
Mirjalol03 <jabborovmirjalol03@gmail.com>
2023-08-07 16:34:18 +0500

    Response added to User Interface and also added Button Logic with reducer

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 
 const Counter = () => {
+  //I'm using useReducer here, not Using Any State Management's 
+  
   const initialState = { count: 0 };
+
   const reducer = (state, action) => {
     switch (action.type) {
       case "increment":
         return { count: state.count + 1 };
       case "decrement":
-        return { count: state.count - 1 };
+        return { count: state.count > 0 ? state.count - 1 : state.count};
       case "reset":
         return initialState;
       default:
         return state;
     }
   };
+  
   const [state, dispatch] = React.useReducer(reducer, initialState);
+
   return (
     <div>
-      <div className="row d-flex justify-content-center ">
-        <div className="col-3">
+      <div className="row justify-content-center">
+        <div className="col-8 col-md-6 col-lg-3">
           <div className="card text-center">
             <div className="card-header">
               <h5>Counter</h5>
