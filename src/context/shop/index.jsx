fcb21e3f79c87df327273673bfb9dbaa87f4bf5c
Mirjalol03 <jabborovmirjalol03@gmail.com>
2023-08-07 16:31:54 +0500

    Code is More clean now

--- src/context/shop/index.jsx
+++ src/context/shop/index.jsx
@@ -4,17 +4,15 @@ import { shopReducer } from "./reducer";
 const Context = React.createContext();
 
 const initialState = {
-    products: [],
-}
+  products: [],
+};
 
 const Provider = ({ children }) => {
-    const [state, dispatch] = React.useReducer(shopReducer, initialState);
-    
-    return (
-        <Context.Provider value={{state, dispatch}}>
-        {children}
-        </Context.Provider  >
-    );
-}
+  const [state, dispatch] = React.useReducer(shopReducer, initialState);
+
+  return (
+    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+  );
+};
 
 export { Context, Provider };
