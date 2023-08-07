25c2c4cfe9e5ac8e2f974109f40c82d95f0e959a
Mirjalol03 <jabborovmirjalol03@gmail.com>
2023-08-07 16:30:57 +0500

    added / to root, when User is on / route, it will navigate to /counter.

--- src/Root/index.jsx
+++ src/Root/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Counter from '../components/counter'
+import { Navigate, Route, Routes } from 'react-router-dom'
+// import Counter from '../components/counter'
 import { navbar } from '../utils/Navbar'
 import Navbar from '../components/Navbar'
 
 const Root = () => {
   return (
     <>
-    <Navbar>    </Navbar>
+    <Navbar/>
       <Routes>
-        {/* <Route path="/counter" element={<Counter />} /> */}
+        <Route path='/' element={<Navigate to={'/counter'}/>}></Route>
         {navbar.length > 0 && (
            navbar.map((item) => (
             <Route key={item.id} path={item.path} element={item.element} />
